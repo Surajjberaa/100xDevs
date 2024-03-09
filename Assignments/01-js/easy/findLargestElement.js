4ae08d64b8aba314b34f20f1ee01c122a64743b5
Suraj Bera <Surajjberaa@users.noreply.github.com>
2024-03-09 16:11:05 +0530

    Completed findLargestElement Assignment

--- Assignments/01-js/easy/findLargestElement.js
+++ Assignments/01-js/easy/findLargestElement.js
@@ -5,8 +5,30 @@
   - Output: 9
 */
 
+
+
 function findLargestElement(numbers) {
+
+    if(numbers == ""){
+        return undefined
+    }
     
+    let largestNum = Number.NEGATIVE_INFINITY
+
+    for (let i = 0; i < numbers.length; i++) {
+    
+    if (numbers[i] > largestNum) {
+        largestNum = numbers[i]        
+    }
+        //  console.log(largestNum);
+    }
+
+    
+    console.log(largestNum);
+    return largestNum
 }
+Input= [-3, -7,- 1, -9, -7]
+
+findLargestElement(Input)
 
 module.exports = findLargestElement;
