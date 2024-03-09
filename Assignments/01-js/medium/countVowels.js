e16326bab825dffca00adb05705d9a06fe6644aa
Suraj Bera <Surajjberaa@users.noreply.github.com>
2024-03-10 00:03:54 +0530

    countVowels Assignment completed

--- Assignments/01-js/medium/countVowels.js
+++ Assignments/01-js/medium/countVowels.js
@@ -7,6 +7,53 @@
 
 function countVowels(str) {
     // Your code here
+
+    let counter = 0
+    const arr = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U',   ] 
+
+      for (let char of str) {
+        if (arr.includes(char)) {
+          
+          counter ++
+        }
+      }
+    
+    console.log(counter);
+    return counter
 }
 
+countVowels("hi hello how WHAT are a a a you")
+
 module.exports = countVowels;
+
+
+// Tried Shit ⬇
+
+ // const arr = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U',   ] 
+
+      // const vowels = () => {
+      //   for (let i = 0; i < arr.length; i++) {
+      //     console.log(arr[i]);
+      //     return arr[i]
+          
+          
+      //   }
+      // }
+
+
+      
+      // if (str[i] == "a" || str[i] == "A" ) {
+      //   counter += 1
+      // }
+      // if (str[i] == "e" || str[i] == "E" ) {
+      //   counter += 1
+      // }
+      // if (str[i] == "i" || str[i] == "I" ) {
+      //   counter += 1
+      // }
+      // if (str[i] == "o" || str[i] == "O" ) {
+      //   counter += 1
+      // }
+      // if (str[i] == "u" || str[i] == "U" ) {
+      //   counter += 1
+      // }
