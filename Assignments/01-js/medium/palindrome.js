cb47bf47d8067f6c512adae5333c6982347f62f7
Suraj Bera <Surajjberaa@users.noreply.github.com>
2024-03-10 00:45:04 +0530

    Completed Palindrome Assignment

--- Assignments/01-js/medium/palindrome.js
+++ Assignments/01-js/medium/palindrome.js
@@ -4,7 +4,23 @@
 */
 
 function isPalindrome(str) {
-  return true;
+
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  if (cleanedStr.length <= 1) {
+    return true
+    
+  }
+
+  const word = cleanedStr.split('').reverse().join('');
+
+  console.log(word);
+  return word.toLowerCase() === cleanedStr.toLowerCase();
+
 }
 
+isPalindrome('race car')
+
 module.exports = isPalindrome;
+
+
