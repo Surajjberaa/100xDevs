e9aba9a3a5cd7480707b501e82a0b95eb7a07a37
Suraj Bera <Surajjberaa@users.noreply.github.com>
2024-03-21 22:05:55 +0530

    Completed Assignments

--- Assignments/week-3/01-middlewares/02-ratelimitter.js
+++ Assignments/week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,22 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req, res, next)=> {
+  const user = req.header['user-id']
+  if (numberOfRequestsForUser[user]) {
+    numberOfRequestsForUser[user]++
+    if(numberOfRequestsForUser[user] > 5){
+      res.status(404).send("Limit Exceeded")
+    }else{
+      next();
+
+    }
+  }else{
+    numberOfRequestsForUser[user] = 1
+    next();
+  }
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
